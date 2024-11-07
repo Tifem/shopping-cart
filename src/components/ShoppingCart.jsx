f27f7090ff208fadde438c52ffb9eca6ee99e510
Emmanuel Boluwatife Janet <88049758+Tifem@users.noreply.github.com>
2024-11-06 23:19:02 -0800

    styling my table

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,64 +1,80 @@
-import React from 'react'
-import Summer from '../assets/images/summer.jpg'
+import React from "react";
+import Summer from "../assets/images/summer.jpg";
+import Bluetooth from "../assets/images/bluetooth.jpeg";
+import Headphone from "../assets/images/headphone.jpeg";
 
 const ShoppingCart = () => {
-
-    const cartItem = [
-        {
-            id:1,
-            name: "Summer T-shirt",
-            price: 15.99,
-            quantity: 2,
-            image:Summer
-
-        },
-        {
-            id:2,
-            name: "Winter T-shirt",
-            price: 15.99,
-            quantity: 1,
-            image:Summer
-        },
-        {
-            id:3,
-            name: "Spring T-shirt",
-            price: 15.99,
-            quantity: 1,
-            image:Summer
-        },
-        {
-            id:4,
-            name: "Autumn T-shirt",
-            price: 15.99,
-            quantity: 1,
-            image:Summer
-        },
-        {
-            id:5,
-            name: "Summer T-shirt",
-            price: 15.99,
-            quantity: 1,
-            image:Summer
-        }
-    ]
+  const cartItem = [
+    {
+      id: 1,
+      name: "Summer T-shirt",
+      price: 15.99,
+      quantity: 2,
+      image: Summer,
+    },
+    {
+      id: 2,
+      name: "Bluetooth",
+      price: 15.99,
+      quantity: 1,
+      image: Bluetooth,
+    },
+    {
+      id: 3,
+      name: "Headphone",
+      price: 15.99,
+      quantity: 1,
+      image: Headphone,
+    },
+    {
+      id: 4,
+      name: "Autumn T-shirt",
+      price: 15.99,
+      quantity: 1,
+      image: Summer,
+    },
+    {
+      id: 5,
+      name: "Summer T-shirt",
+      price: 15.99,
+      quantity: 1,
+      image: Summer,
+    },
+  ];
   return (
-    <div className='container'>
+    <div className="container">
       <h1>Shooping Cart</h1>
-      <ul>
-       {cartItem.map((item,index) =>{
-            return (
-              <li key={index}>
-                <img src={item.image} alt={item.name} className='image'/>
-                <div>ID: {item.id}</div>
-                <div>Name: {item.name}</div>
-                <div>Price: ${item.price.toFixed(2)}</div>
-                <div>Quantity: {item.quantity}</div>
-              </li>
-            );
-       })}
-      </ul>
+      <div className="items">
+        <table>
+          <thead>
+            <tr>
+              <th>Products</th>
+              <th >Price</th>
+              <th>Quantity</th>
+              <th>Subtotal</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cartItem.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <td>
+                    {" "}
+                    <img src={item.image} alt={item.name} className="image" />
+                    <span>{item.name}</span>
+                  </td>
+                  {/* <td>{item.id}</td> */}
+                  <td>${item.price.toFixed(2)}</td>
+                  <td>{item.quantity}</td>
+                  <td>{item.price*item.quantity}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
-}
+};
 
-export default ShoppingCart
+export default ShoppingCart;
